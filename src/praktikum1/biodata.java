388f6315963bd75e3e2157204b18f351a9b09d86
atikafitriani <60200112003@uin-alauddin.ac.id>
2015-04-21 17:24:22 +0800

    laporan1

---
+++ src/praktikum1/biodata.java
@@ -0,0 +1,10 @@
+package praktikum1;
+
+public class biodata {
+	
+	void bio(){
+	System.out.println("atikafitriani");
+	System.out.println("21021994");
+	System.out.println("asrama rusunawa");
+	}
+}
---
+++ src/praktikum1/penjumlahan.java
@@ -0,0 +1,11 @@
+package praktikum1;
+
+public class penjumlahan {
+	void tambah(){
+		int a;
+		
+		a=1+1;
+		System.out.print("1+1= ");
+		System.out.println(a);
+	}
+}
